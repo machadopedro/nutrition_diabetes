20d64ed8e3a20b86f61cb4d26307e7b28db9640b
Pedro Goncalves <pedro.goncalves@dasa.com.br>
2024-07-17 19:23:39 -0300

    nutrition table update and layout fixing

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import pandas as pd
 import time
 
 df = pd.read_csv('nutrition_table.csv')
-# df = df[['Categoria', 'Alimento (100 g)', 'Carboidratos (g)', 'Proteínas (g)',
-#        'Gorduras (g)', 'Índice Glicêmico', 'Calorias (kcal)']]
 
 st.title("Calculo de Refeição")
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
+
 def click_button():
-    st.session_state.clicked = True
+    refeicao = pd.DataFrame(
+            columns=["Alimento", "Quantidade (g)", "Carboidratos líquidos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
+        )
+    st.session_state.refeicao = refeicao
+
 peso = st.sidebar.number_input(label="Peso do paciente", value=70, key="peso")
 
 fator_correcao = st.sidebar.number_input(label="Fator de correção", value=1.0, step=0.1, key="fator_correcao")
@@ -21,14 +22,10 @@ st.number_input(label="Quantidade (g)", value=100, key="quantidade")
 
 if 'refeicao' not in st.session_state:
     refeicao = pd.DataFrame(
-            columns=["Alimento", "Quantidade (g)", "Carboidratos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
+            columns=["Alimento", "Quantidade (g)", "Carboidratos líquidos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
         )
     st.session_state.refeicao = refeicao
-if 'final_df' not in st.session_state:
-    final_df = pd.DataFrame(
-                columns=["Alimento", "Quantidade (g)", "Carboidratos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
-            )
-    st.session_state.final_df = final_df
+
 if st.button('Adicionar Alimento'):
     if st.session_state.quantidade <= 0:
         st.warning("Quantidade deve ser maior que 0", icon="⚠️")
@@ -39,101 +36,75 @@ if st.button('Adicionar Alimento'):
         new_row = {
             "Alimento": alimento_infos['Alimento (100 g)'],
             "Quantidade (g)": st.session_state.quantidade,
-            "Carboidratos (g)": alimento_infos['Carboidratos (g)'] * st.session_state.quantidade/100,
+            "Carboidratos líquidos (g)": alimento_infos['Carboidratos líquidos (g)'] * st.session_state.quantidade/100,
             "Proteínas (g)": alimento_infos['Proteínas (g)'] * st.session_state.quantidade/100,
             "Gorduras (g)": alimento_infos['Gorduras (g)'] * st.session_state.quantidade/100,
             "Calorias (kcal)": alimento_infos['Calorias (kcal)'] * st.session_state.quantidade/100,
-            "Insulina UI": peso * (alimento_infos['Carboidratos (g)']/10 + alimento_infos['Proteínas (g)']/50 + alimento_infos['Gorduras (g)']/100) * fator_correcao/70 * st.session_state.quantidade/100
+            "Insulina UI": st.session_state.peso * (alimento_infos['Carboidratos líquidos (g)']/10 + alimento_infos['Proteínas (g)']/50 + alimento_infos['Gorduras (g)']/100) * st.session_state.fator_correcao/70 * st.session_state.quantidade/100
         }
-        st.session_state.refeicao = pd.concat([st.session_state.refeicao, pd.DataFrame(new_row)], ignore_index=True)
-        st.session_state.final_df = st.session_state.refeicao.copy()
+        # Remove the 'Total' row if it exists
+        if 'Total' in st.session_state.refeicao['Alimento'].values:
+            st.session_state.refeicao = st.session_state.refeicao[st.session_state.refeicao['Alimento'] != 'Total']
+
+        if len(st.session_state.refeicao) > 0 and new_row["Alimento"].values[0] in st.session_state.refeicao["Alimento"].values:
+            matching_index = st.session_state.refeicao[st.session_state.refeicao["Alimento"] == new_row["Alimento"].values[0]].index[0]
+
+            # Update the values using the matching index
+            st.session_state.refeicao.loc[matching_index, "Quantidade (g)"] += st.session_state.quantidade
+            st.session_state.refeicao.loc[matching_index, "Carboidratos líquidos (g)"] += new_row["Carboidratos líquidos (g)"].values[0]  # Access the value directly
+            st.session_state.refeicao.loc[matching_index, "Proteínas (g)"] += new_row["Proteínas (g)"].values[0]
+            st.session_state.refeicao.loc[matching_index, "Gorduras (g)"] += new_row["Gorduras (g)"].values[0]
+            st.session_state.refeicao.loc[matching_index, "Calorias (kcal)"] += new_row["Calorias (kcal)"].values[0]
+            st.session_state.refeicao.loc[matching_index, "Insulina UI"] += new_row["Insulina UI"].values[0]
+        else:
+            st.session_state.refeicao = pd.concat([st.session_state.refeicao, pd.DataFrame(new_row)], ignore_index=True)
+
         soma = [{
             "Alimento": 'Total',
-            "Quantidade (g)": sum(st.session_state.final_df['Quantidade (g)']),
-            "Carboidratos (g)": sum(st.session_state.final_df['Carboidratos (g)']),
-            "Proteínas (g)": sum(st.session_state.final_df['Proteínas (g)']),
-            "Gorduras (g)": sum(st.session_state.final_df['Gorduras (g)']),
-            "Calorias (kcal)": sum(st.session_state.final_df['Calorias (kcal)']),
-            "Insulina UI": sum(st.session_state.final_df['Insulina UI'])
+            "Quantidade (g)": sum(st.session_state.refeicao['Quantidade (g)']),
+            "Carboidratos líquidos (g)": sum(st.session_state.refeicao['Carboidratos líquidos (g)']),
+            "Proteínas (g)": sum(st.session_state.refeicao['Proteínas (g)']),
+            "Gorduras (g)": sum(st.session_state.refeicao['Gorduras (g)']),
+            "Calorias (kcal)": sum(st.session_state.refeicao['Calorias (kcal)']),
+            "Insulina UI": sum(st.session_state.refeicao['Insulina UI'])
         }]
-        st.session_state.final_df = pd.concat([st.session_state.final_df, pd.DataFrame(soma)], ignore_index=True)
+        # st.session_state.refeicao.loc['Total']= st.session_state.refeicao.sum(numeric_only=True, axis=0)
+        st.session_state.refeicao = pd.concat([st.session_state.refeicao, pd.DataFrame(soma)], ignore_index=True)
+        # st.data_editor(st.session_state.refeicao)
 st.subheader("Refeição")
-st.dataframe(st.session_state.final_df, hide_index=True, use_container_width=True, on_select="ignore")
 
+def update():
+    alimento_editado_index = list(st.session_state.editor['edited_rows'].keys())[0]
+    alimento_nome = st.session_state.refeicao.iloc[alimento_editado_index, 0]  # Get the food name from the edited row
+    alimento_infos = df[df["Alimento (100 g)"] == alimento_nome]  # Compare against the food name
+    quantidade = st.session_state.editor['edited_rows'][alimento_editado_index]["Quantidade (g)"]
+    st.session_state.refeicao.loc[alimento_editado_index, "Carboidratos líquidos (g)"] = alimento_infos['Carboidratos líquidos (g)'].values[0] * quantidade/100
+    st.session_state.refeicao.loc[alimento_editado_index, "Proteínas (g)"] = alimento_infos['Proteínas (g)'].values[0] * quantidade/100
+    st.session_state.refeicao.loc[alimento_editado_index, "Gorduras (g)"] = alimento_infos['Gorduras (g)'].values[0] * quantidade/100
+    st.session_state.refeicao.loc[alimento_editado_index, "Calorias (kcal)"] = alimento_infos['Calorias (kcal)'].values[0] * quantidade/100
+    st.session_state.refeicao.loc[alimento_editado_index, "Insulina UI"] = st.session_state.peso * (alimento_infos['Carboidratos líquidos (g)'].values[0]/10 + alimento_infos['Proteínas (g)'].values[0]/50 + alimento_infos['Gorduras (g)'].values[0]/100) * st.session_state.fator_correcao/70 * quantidade/100
+    st.session_state.refeicao.loc[alimento_editado_index, "Quantidade (g)"] = quantidade
+    # Remove the 'Total' row if it exists
+    if 'Total' in st.session_state.refeicao['Alimento'].values:
+        st.session_state.refeicao = st.session_state.refeicao[st.session_state.refeicao['Alimento'] != 'Total']
+    soma = [{
+            "Alimento": 'Total',
+            "Quantidade (g)": sum(st.session_state.refeicao['Quantidade (g)']),
+            "Carboidratos líquidos (g)": sum(st.session_state.refeicao['Carboidratos líquidos (g)']),
+            "Proteínas (g)": sum(st.session_state.refeicao['Proteínas (g)']),
+            "Gorduras (g)": sum(st.session_state.refeicao['Gorduras (g)']),
+            "Calorias (kcal)": sum(st.session_state.refeicao['Calorias (kcal)']),
+            "Insulina UI": sum(st.session_state.refeicao['Insulina UI'])
+        }]
+        # st.session_state.refeicao.loc['Total']= st.session_state.refeicao.sum(numeric_only=True, axis=0)
+    st.session_state.refeicao = pd.concat([st.session_state.refeicao, pd.DataFrame(soma)], ignore_index=True)
+columns = list(st.session_state.refeicao.columns)
+columns.remove('Quantidade (g)')
 
+st.data_editor(st.session_state.refeicao, hide_index=True, key='editor', on_change=update, use_container_width=True, disabled=set(columns))
 
-if len(st.session_state.final_df) > 0:
+if len(st.session_state.refeicao) > 0:
     st.button('Nova refeição', on_click=click_button)
-
-if st.session_state.clicked:
-    refeicao = pd.DataFrame(
-            columns=["Alimento", "Quantidade (g)", "Carboidratos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
-        )
-    st.session_state.refeicao = refeicao
-    st.session_state.final_df = refeicao.copy()
-    st.session_state.clicked = False
-    st.rerun()
-# text_search = st.text_input("Busque alimentos", value="")
-# alimento_processed = df["Alimento (100 g)"].str.normalize('NFKD').str.encode('ascii',errors='ignore').str.decode('utf-8').str.lower()
-# m1 = alimento_processed.str.contains(str.lower(text_search))
-# df_search = df[m1]
-
-# N_cards_per_row = 3
-# if text_search:
-
-#     for n_row, row in df_search.reset_index().iterrows():
-#         i = n_row%N_cards_per_row
-#         if i==0:
-#             st.write("---")
-#             cols = st.columns(N_cards_per_row, gap="large")
-#         # draw the card
-#         with cols[n_row%N_cards_per_row]:
-#             st.caption(f"{row['Categoria'].strip()} ")
-#             st.markdown(f"**{row['Alimento (100 g)'].strip()}**")
-#             st.markdown(f"*{row['Calorias (kcal)']}*")
-#             # st.markdown(f"**{row['Video']}**")
-
-
 
 st.header("Tabela Nutricional")
 event = st.dataframe(df.astype(str), hide_index=True, use_container_width=True,)
-
-# meal_df = pd.DataFrame(
-#             columns=["Alimento", "Quantidade (g)", "Carboidratos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
-#         )
-# if 'meal_df' not in st.session_state:
-#     st.session_state.meal_df = meal_df
-
-
-
-# if st.button('Adicionar Alimento'):
-#     alimento_infos = df.iloc[event.selection.rows]
-#     new_row = {
-#         "Alimento": alimento_infos['Alimento (100 g)'],
-#         "Quantidade (g)": quantidade,
-#         "Carboidratos (g)": alimento_infos['Carboidratos (g)'] * quantidade/100,
-#         "Proteínas (g)": alimento_infos['Proteínas (g)'] * quantidade/100,
-#         "Gorduras (g)": alimento_infos['Gorduras (g)'] * quantidade/100,
-#         "Calorias (kcal)": alimento_infos['Calorias (kcal)'] * quantidade/100,
-#         "Insulina UI": peso * (alimento_infos['Carboidratos (g)']/10 + alimento_infos['Proteínas (g)']/50 + alimento_infos['Gorduras (g)']/100) * fator_correcao/70 * quantidade/100
-#     }
-#     st.session_state.meal_df = pd.concat([st.session_state.meal_df, pd.DataFrame(new_row)], ignore_index=True)
-#     final_df = st.session_state.meal_df.copy()
-#     soma = [{
-#         "Alimento": 'SOMA',
-#         "Quantidade (g)": sum(final_df['Quantidade (g)']),
-#         "Carboidratos (g)": sum(final_df['Carboidratos (g)']),
-#         "Proteínas (g)": sum(final_df['Proteínas (g)']),
-#         "Gorduras (g)": sum(final_df['Gorduras (g)']),
-#         "Calorias (kcal)": sum(final_df['Calorias (kcal)']),
-#         "Insulina UI": sum(final_df['Insulina UI'])
-#     }]
-#     final_df = pd.concat([final_df, pd.DataFrame(soma)], ignore_index=True)
-#     st.dataframe(final_df, hide_index=True, use_container_width=True )
-
-#     if st.button('Nova Refeição'):
-#         meal_df = pd.DataFrame(
-#                 columns=["Alimento", "Quantidade (g)", "Carboidratos (g)", "Proteínas (g)", "Gorduras (g)", "Calorias (kcal)", "Insulina UI"]
-#             )
-#         st.session_state.meal_df = meal_df
-
